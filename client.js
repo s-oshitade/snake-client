2bbfe7690727cfa9bc2cde03ec42d7f9ee51ab51
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-22 23:18:35 +0000

    Experiment with setTimeout

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { setInterval } = require("timers");
 
 //// establish a connection with the game server
 const connect = function () {
@@ -11,8 +12,13 @@ const connect = function () {
   //send messages to client console and server upon connection
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
-    conn.write('Name: SO');
-    // conn.write('Hello from client!');
+    conn.write("Name: SO");
+    const durations = [1000, 3000, 5000, 8000, 10000];
+    for (const duration of durations) {
+      setTimeout(() => {
+        conn.write("Move: up");
+      }, duration);
+    }
   });
   //handle incoming data upon connection
   conn.on("data", (data) => {
@@ -23,4 +29,4 @@ const connect = function () {
 
 console.log("Connecting...");
 
-module.exports = { connect }
+module.exports = { connect };
