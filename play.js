600ad182a632b49aa89942cd04ede28008630ac7
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-22 23:40:46 +0000

    Add function setupInput to play.js

--- play.js
+++ play.js
@@ -1,4 +1,11 @@
-const { connect } = require('./client');
+const { connect } = require("./client");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
 connect();
-
