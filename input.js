14e2fe1c2c6a1cdbc599ca8116099fea65320803
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-23 01:04:55 +0000

    Move functions setupInput and handleUserInput to the input module

---
+++ input.js
@@ -0,0 +1,24 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin; //the returned stdin object will allow client to listen for keyboard input and react to it accordingly
+};
+const handleUserInput = function () {
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      // \u0003 maps to ctrl+c input
+      process.exit();
+    }
+    if (key === "placeholder") {
+    }
+    if (key === "placeholder") {
+    }
+    if (key === "placeholder") {
+    }
+    if (key === "placeholder") {
+    }
+  });
+};
