46ddfed11f311e3290e78260234c86eb53589ef2
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-23 20:38:33 +0000

    Handle player initials as command line argument

--- constants.js
+++ constants.js
@@ -1,7 +1,26 @@
+const NAME = "Name: SO"; //@TODO: Update NAME to receive input via user interface such as form/ prompt/ command line argument
+const PLAYER_INITIALS = process.argv.slice(2);
 const IP = "10.0.2.15";
 const PORT = 50541;
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+const DISPLAY_MESSAGE = {
+  c: "Say: Here I come!",
+  x: "Say: Ready or not?!",
+  y: "Say: Yes!!!",
+  z: "Say: Ssssssss",
+};
 
 module.exports = {
+  PLAYER_INITIALS,
+  NAME,
   IP,
   PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_LEFT_KEY,
+  DISPLAY_MESSAGE,
 };
