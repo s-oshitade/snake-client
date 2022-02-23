73410ba52ac48a60fe639f52ece247a802ac46d3
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-23 20:39:48 +0000

    Import and incorporate player initials as command line argument

--- client.js
+++ client.js
@@ -1,18 +1,18 @@
 const net = require("net");
-const { setInterval } = require("timers");
+const { HOST, PORT, PLAYER_INITIALS } = require('./constants');
 
 //// establish a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541,
+    host: HOST,
+    port: PORT,
   });
   //interpret incoming data as text
   conn.setEncoding("utf8");
   //send messages to client console and server upon connection
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
-    conn.write("Name: SO");
+    conn.write(`Name: ${PLAYER_INITIALS}`);
   });
   //handle incoming data upon connection
   conn.on("data", (data) => {
