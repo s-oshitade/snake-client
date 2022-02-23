0ed893dcd4bebade7d3a8da6d61f51492b5f10e4
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-23 00:51:54 +0000

    Set up stdin with functions setupInput and handleUserInput

--- play.js
+++ play.js
@@ -5,7 +5,25 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  return stdin;
+  stdin.on("data", handleUserInput);
+  return stdin; //the returned stdin object will allow client to listen for keyboard input and react to it accordingly
+};
+const handleUserInput = function () {
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      // \u0003 maps to ctrl+c input
+      process.exit();
+    }
+    if (key === "placeholder") {
+    }
+    if (key === "placeholder") {
+    }
+    if (key === "placeholder") {
+    }
+    if (key === "placeholder") {
+    }
+  });
 };
 
 connect();
+setupInput();
